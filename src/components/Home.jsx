df495e7d87f23fa416fd89754e438780c805f0c0
Oluwatoyin <oluwatoyinjolaoso001@gmail>
2023-08-06 20:46:25 +0100

    updated picture

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-scroll'
-import heroImg from '../assets/heroImg.png'
+import heroImg from '../assets/image.jpg'
 import { IoIosArrowForward } from 'react-icons/io'
 
 const Home = () => {
