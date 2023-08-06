be8f930c38b58670d074ac01ca513dd088a0132e
Oluwatoyin <oluwatoyinjolaoso001@gmail>
2023-08-06 21:13:50 +0100

    Added about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,34 @@
 const About = () => {
   return (
-    <div name="about" className="w-full bg-gradient-to-b from-gray-800 to-black text-white">
+    <div
+      name="about"
+      className="w-full bg-gradient-to-b from-gray-800 to-black text-white"
+    >
       <div className="max-w-7xl px-4 pt-24 mx-auto flex flex-col justify-center w-full md:px-8">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
         <p className="text-xl">
-          Hello! I am <strong>Agnes Omolola Oladeinde</strong> from Abuja Nigeria.I am an enthusiastic frontend developer dedicated to creating captivating user experience through innovative web design. I have a strong foundation in HTML, CSS, and JavaScript and I specialize in building responsive, intuitive and interactive websites while optimizing performance and implementing modern frameworks. 
+          I am <strong>Agnes Omolola Oladeinde</strong>, a highly motivated Data
+          Analyst with certifications from DataCamp and ALX. I bring a unique
+          blend of technical proficiency and domain knowledge to the table. I am
+          proficient in SQL, Excel, and Python, I conduct comprehensive data
+          analysis, utilizing techniques such as exploratory data analysis,
+          statistics, and data visualization.
         </p>
 
         <br />
 
         <p className="text-xl">
-        I do my best to bring clients&apos; visions to life while adhering to industry best practices. I am constantly seeking new challenges to expand my skill set and stay ahead in the ever-evolving web development technologies.
+          My dedication to precision and efficiency, coupled with a
+          goal-oriented mindset, empowers me to deliver impactful results and
+          make a positive impact in the healthcare industry.
         </p>
       </div>
     </div>
-  )
+  );
 }
 
 export default About
